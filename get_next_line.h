beabbc51312f421a5d3811a641caa0f3fe73fed3
Julien Bernard <jbernard@c2r1p3.42quebec.com>
2021-10-19 13:19:01 -0400

    Final Form

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbernard <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 10:12:00 by jbernard          #+#    #+#             */
+/*   Updated: 2021/10/19 10:12:01 by jbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -5,17 +17,13 @@
 # include <unistd.h>
 
 /* utils */
-int	    ft_strlen_char(char *str, char c);
+int		ft_strlen_char(char *str, char c);
 char	*ft_strdup(char *str);
 char	*ft_joinfree(char *s1, char *s2);
 
 /* get_next_line */
-int     read_next_buffer(char **save, int fd);
-void    stream_content(char **save, char **line, int eol_position);
+int		read_next_buffer(char **save, int fd);
+void	stream_content(char **save, char **line, int eol_position);
 char	*get_next_line(int fd);
 
-
-# include <fcntl.h>
-# include <stdio.h>
-int main();
 #endif
